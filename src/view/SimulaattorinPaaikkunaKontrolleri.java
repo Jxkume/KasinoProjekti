b25635fe70b81f59c3f287d48b451e46d57cf1f7
Valdo <valttku@metropolia.fi>
2023-02-26 19:13:16 +0200

    Muokattu kaynnistaSimulointi-metodia. Simulaatio voidaan käynnistää vain, jos käyttäjä on syöttänyt arvoja ja arvot eivät ole negatiivisia. Lisäksi käynnistyksen jälkeen Käynnistä-nappi lakkaa toimimasta, jotta käyttäjä voi suorittaa simulaatioajon vain kerran. Muokattu myös nopeuta-metodia, varmistin ettei viive voi mennä negatiiviseksi sitä nopeutettaessa.

--- src/view/SimulaattorinPaaikkunaKontrolleri.java
+++ src/view/SimulaattorinPaaikkunaKontrolleri.java
@@ -2,11 +2,14 @@ package view;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import main.SimulaattoriMain;
 import simu.framework.IMoottori;
+import simu.framework.Kello;
 import simu.model.OmaMoottori;
 
 public class SimulaattorinPaaikkunaKontrolleri {
@@ -42,19 +45,56 @@ public class SimulaattorinPaaikkunaKontrolleri {
     }
     
 	public void kaynnistaSimulointi() {
-		moottori = new OmaMoottori(this); // luodaan uusi moottorisäie jokaista simulointia varten
-		moottori.setSimulointiaika(getAika());
-		moottori.setViive(getViive());
-		//ui.getVisualisointi().tyhjennaNaytto();
-		((Thread)moottori).start();
+		Alert alert = new Alert(AlertType.ERROR);
+		// Tarkistetaan, että käyttäjä on syöttänyt simulointiajan ja viiveen
+		if (aika.getText().isEmpty() && viive.getText().isEmpty()) {
+			alert.setTitle("Simulointiaika ja viive puuttuvat!");
+	        alert.setHeaderText("Ole hyvä ja aseta simulointiaika ja viive käynnistääksesi simulaattorin.");
+	        alert.showAndWait();
+		} else if (aika.getText().isEmpty()) {
+	        alert.setTitle("Simulointiaika puuttuu!");
+	        alert.setHeaderText("Ole hyvä ja aseta simulointiaika käynnistääksesi simulaattorin.");
+	        alert.showAndWait();
+		} else if (viive.getText().isEmpty()) {
+	        alert.setTitle("Viive puuttuu!");
+	        alert.setHeaderText("Ole hyvä ja aseta viive käynnistääksesi simulaattorin.");
+	        alert.showAndWait();
+		} else {
+			// Tarkistetaan, että arvot eivät ole negatiivisia
+			if (Integer.parseInt(aika.getText()) < 0) {
+		        alert.setTitle("Negatiivinen simulointiaika!");
+		        alert.setHeaderText("Simulointiaika ei voi olla negatiivinen.");
+		        alert.showAndWait();
+			} else if (Integer.parseInt(viive.getText()) < 0) {
+		        alert.setTitle("Negatiivinen viive!");
+		        alert.setHeaderText("Viive ei voi olla negatiivinen.");
+		        alert.showAndWait();
+			} else {
+				// Simulaatio voidaan käynnistää
+				moottori = new OmaMoottori(this);
+				moottori.setSimulointiaika(getAika());
+				moottori.setViive(getViive());
+				//ui.getVisualisointi().tyhjennaNaytto();
+				((Thread)moottori).start();
+				// Nappia voi painaa vain kerran
+				kaynnistaButton.setDisable(true);
+			}
+		}
 	}
 	
 	public void hidasta() { // hidastetaan moottorisäiettä
+		// Lisätään viiveeseen 10
 		moottori.setViive((long)(moottori.getViive() + 10));
 	}
 	
 	public void nopeuta() { // nopeutetaan moottorisäiettä
-		moottori.setViive((long)(moottori.getViive() - 10));
+		// Varmistetaan ettei viive voi mennä negatiiviseksi
+		if (moottori.getViive() < 10) {
+			moottori.setViive(0);
+		} else {
+			// Vähennetään viiveestä 10
+			moottori.setViive((long)(moottori.getViive() - 10));
+		}
 	}
 	
 	public void naytaLoppuaika(double aika) {
